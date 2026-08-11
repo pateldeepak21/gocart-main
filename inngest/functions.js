5e2b079235af0a8483fa5ec9fa2eec3d50d0ff38
pateldeepak21 <dp4779832@gmail.com>
2026-08-11 19:57:11 +0530

    commit change

--- inngest/functions.js
+++ inngest/functions.js
@@ -1,9 +1,8 @@
 import { inngest } from './client'
 import prisma from '@/lib/prisma'
- 
+
 export const syncUserCreation = inngest.createFunction(
-    { id: 'sync-user-create' },
-    { event: 'clerk/user.created' },
+    { id: 'sync-user-create', triggers: { event: 'clerk/user.created' } },
     async ({ event }) => {
         const { data } = event
         await prisma.user.create({
@@ -16,16 +15,14 @@ export const syncUserCreation = inngest.createFunction(
         })
     }
 )
- 
+
 export const syncUserUpdation = inngest.createFunction(
-    { id: 'sync-user-update' },
-    { event: 'clerk/user.updated' },
+    { id: 'sync-user-update', triggers: { event: 'clerk/user.updated' } },
     async ({ event }) => {
         const { data } = event
         await prisma.user.update({
             where: { id: data.id },
             data: {
-                id: data.id,
                 email: data.email_addresses[0].email_address,
                 name: `${data.first_name} ${data.last_name}`,
                 image: data.image_url,
@@ -33,10 +30,9 @@ export const syncUserUpdation = inngest.createFunction(
         })
     }
 )
- 
+
 export const syncUserDeletion = inngest.createFunction(
-    { id: 'sync-user-delete' },
-    { event: 'clerk/user.deleted' },
+    { id: 'sync-user-delete', triggers: { event: 'clerk/user.deleted' } },
     async ({ event }) => {
         const { data } = event
         await prisma.user.delete({
